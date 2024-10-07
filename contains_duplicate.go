4579f0bb0f309d326213516c5d6bc323976bb401
unknown <amarpitmalik204@gmail.com>
2024-10-07 22:34:55 +0530

    contains duplicate

---
+++ contains_duplicate.go
@@ -0,0 +1,17 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	input := []int{2, 1, 5, 4, 5}
+// 	set := make(map[int]int)
+
+// 	for i := 0; i <= len(input)-1; i++ {
+// 		if _, exists := set[input[i]]; exists {
+// 			fmt.Println("True")
+// 			break
+// 		} else {
+// 			set[input[i]] = input[i]
+// 		}
+// 	}
+// }
--- twosum.go
+++ twosum.go
@@ -1,22 +1,22 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	input := []int{2, 1, 3, 5, 8}
-	target := 9
+// func main() {
+// 	input := []int{2, 1, 3, 5, 8}
+// 	target := 9
 
-	set := make(map[int]int)
+// 	set := make(map[int]int)
 
-	for i := 0; i <= len(input)-1; i++ {
+// 	for i := 0; i <= len(input)-1; i++ {
 
-		data := target - input[i]
+// 		data := target - input[i]
 
-		if _, exists := set[data]; exists {
-			fmt.Println(i, set[data])
-		} else {
-			set[i] = input[i]
-		}
+// 		if _, exists := set[data]; exists {
+// 			fmt.Println(i, set[data])
+// 		} else {
+// 			set[i] = input[i]
+// 		}
 
-	}
-}
+// 	}
+// }
