2f12c556690927cfcf16ab6ccc5ef376b6b7bfc5
unknown <amarpitmalik204@gmail.com>
2024-11-12 23:35:04 +0530

    remove nth element

--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"arrayMain"
-	"fmt"
 	"ll"
-	"lru"
-	"stringMain"
 )
 
 func main() {
-	fmt.Println("Start Running DSA Programs")
-	arrayMain.Array()
-	fmt.Println("Completed Running Array Programs")
-	stringMain.String()
-	fmt.Println("Completed Running String Programs")
-	lru.LRUCache()
-	fmt.Println("Printed LRU Cache")
-	ll.DetectCycle()
-	ll.ReverseList()
+	// fmt.Println("Start Running DSA Programs")
+	// arrayMain.Array()
+	// fmt.Println("Completed Running Array Programs")
+	// stringMain.String()
+	// fmt.Println("Completed Running String Programs")
+	// lru.LRUCache()
+	// fmt.Println("Printed LRU Cache")
+	// ll.DetectCycle()
+	// ll.ReverseList()
+	ll.RemoveEnd()
 }
