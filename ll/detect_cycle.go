b07f56ac5385c7dd0b2ff503cafbee012bd39d8a
unknown <amarpitmalik204@gmail.com>
2024-11-12 22:45:19 +0530

    detect cycle

--- ll/detect_cycle.go
+++ ll/detect_cycle.go
@@ -33,3 +33,25 @@ func DetectCycle() {
 	head := createList(data)
 	PrintList(head)
 }
+
+func hasCycle(head *Node) bool {
+	if head == nil || head.next == nil {
+		return false
+	}
+	slow := head
+	fast := head
+	result := false
+	for fast != nil && fast.next != nil {
+		if fast == nil || fast.next == nil {
+			break
+		}
+		fast = fast.next.next
+		slow = slow.next
+		if fast == slow {
+			result = true
+			break
+		}
+
+	}
+	return result
+}
