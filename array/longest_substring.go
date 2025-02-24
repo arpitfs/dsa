9538de280623e090b4a5df70c56c484071735de8
unknown <amarpitmalik204@gmail.com>
2025-02-25 00:08:59 +0530

    lwrc

--- array/longest_substring.go
+++ array/longest_substring.go
@@ -1,5 +1,25 @@
 package array
 
+import "fmt"
+
 func lwrc() {
+
+	input := "abba"
+	left := 0
+	ans := 0
+	tracker := make(map[rune]int)
+
+	for right := 0; right < len(input); right++ {
+		if _, e := tracker[rune(input[right])]; !e {
+			tracker[rune(input[right])] = right
+		} else {
+			left = max(left, tracker[rune(input[right])]+1)
+			tracker[rune(input[right])] = right
+		}
+
+		ans = max(ans, right-left+1)
+	}
+
+	fmt.Println(ans)
 
 }
