c4f97302eb303a0bb83f00206b5a415ab8dc69ba
unknown <amarpitmalik204@gmail.com>
2025-04-01 23:29:29 +0530

    test

---
+++ practice/sliding-window/min-in-sw.go
@@ -0,0 +1,10 @@
+package slidingwindow
+
+// // Find the smallest substring containing all characters of another string.
+// // s = "ADOBECODEBANC"
+// // t = "ABC"
+
+// func MinSubstringInWindow() {
+// 	input := "ADOBECODEBANC"
+// 	subString := "ABC"
+// }
